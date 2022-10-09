9ab5889cc900ed556afd68e24a39babd2d73ddad
Nadilson <nadilson@protonmail.com>
2022-10-09 07:03:46 -0300

    Recuperando parâmetros das rotas

--- src/app/pages/portfolio/card/card.component.ts
+++ src/app/pages/portfolio/card/card.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private parametrizador: ActivatedRoute) {
+    this.parametrizador.params.subscribe((res) => console.log(res));
   }
 
+  ngOnInit(): void {}
 }
