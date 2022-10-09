174adb062b5d0efc27ccc8bdc2bc4cfe30e8d523
Nadilson <nadilson@protonmail.com>
2022-10-09 06:29:03 -0300

    Primeiras Rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TitleComponent } from './pages/index/title/title.component';
+import { CardComponent } from './pages/portfolio/card/card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TitleComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'portfolio',
+    component: CardComponent,
+    pathMatch: 'prefix',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
